26415e4ecf7b61054d8b96270a7a2d7a1ffd6c09
Vernicovskiy <victor02vernicovskiy@gmail.com>
2023-10-26 09:05:16 +0300

    Added Blinking Led CMSIS project for analysis

---
+++ test_pr_CMSIS/Debug/CMSIS/src/system_stm32f4xx.d
@@ -0,0 +1,17 @@
+CMSIS/src/system_stm32f4xx.o: ../CMSIS/src/system_stm32f4xx.c \
+ D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/stm32f4xx.h \
+ D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/stm32f401xe.h \
+ D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/core_cm4.h \
+ D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_version.h \
+ D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_compiler.h \
+ D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_gcc.h \
+ D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/mpu_armv7.h \
+ D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/system_stm32f4xx.h
+D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/stm32f4xx.h:
+D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/stm32f401xe.h:
+D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/core_cm4.h:
+D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_version.h:
+D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_compiler.h:
+D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_gcc.h:
+D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/mpu_armv7.h:
+D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/system_stm32f4xx.h:
