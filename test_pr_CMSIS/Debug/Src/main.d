a63deee13a70124d605035fd654217969fa7aea4
Vernicovskiy <victor02vernicovskiy@gmail.com>
2023-10-26 09:08:55 +0300

    Added Blinking Led LL project for analysis

--- test_pr_CMSIS/Debug/Src/main.d
+++ test_pr_CMSIS/Debug/Src/main.d
@@ -1,18 +1,18 @@
 Src/main.o: ../Src/main.c ../Inc/main.h \
- D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/stm32f4xx.h \
- D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/stm32f401xe.h \
- D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/core_cm4.h \
- D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_version.h \
- D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_compiler.h \
- D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_gcc.h \
- D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/mpu_armv7.h \
- D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/system_stm32f4xx.h
+ D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/stm32f4xx.h \
+ D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/stm32f401xe.h \
+ D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/core_cm4.h \
+ D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/cmsis_version.h \
+ D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/cmsis_compiler.h \
+ D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/cmsis_gcc.h \
+ D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/mpu_armv7.h \
+ D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/system_stm32f4xx.h
 ../Inc/main.h:
-D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/stm32f4xx.h:
-D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/stm32f401xe.h:
-D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/core_cm4.h:
-D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_version.h:
-D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_compiler.h:
-D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/cmsis_gcc.h:
-D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/mpu_armv7.h:
-D:/WORK/WORKSPACE\ for\ CUBE\ IDE\ 1.13.2/test_pr_CMSIS/CMSIS/inc/system_stm32f4xx.h:
+D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/stm32f4xx.h:
+D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/stm32f401xe.h:
+D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/core_cm4.h:
+D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/cmsis_version.h:
+D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/cmsis_compiler.h:
+D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/cmsis_gcc.h:
+D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/mpu_armv7.h:
+D:/Nero/STM32-library-analysis/test_pr_CMSIS/CMSIS/inc/system_stm32f4xx.h:
